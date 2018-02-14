b2d3f2deddabcfb0620930a676855e7c44bc24e9
Juha-Matti Tirilä <juha-matti.tirila@op.fi>
2018-02-14 17:05:44 +0200

    Start implementing learning

--- classifier.cpp
+++ classifier.cpp
@@ -17,6 +17,44 @@ GNB::~GNB() {}
 void GNB::train(vector<vector<double>> data, vector<string> labels)
 {
 
+  // TODO: implement these so the avg's are computer separately for every label, use vector instead of individual double's
+
+  int data_size = data.size();
+
+  double s_sum;
+  double d_sum;
+  double s_dots_sum;
+  double d_dots_sum;
+
+  double s_avg;
+  double d_avg;
+
+  double s_dots_avg;
+  double d_dots_avg;
+
+
+  for(int i = 0; i < data_size; i++) {
+    s_sum += data[i][0];
+    d_sum += data[i][1];
+    s_dots_sum += data[i][2];
+    d_dots_sum += data[i][3];
+  }
+
+
+  s_avg = s_sum / data_size;
+  d_avg = d_sum / data_size;
+  s_dots_avg = s_dots_sum / data_size;
+  d_dots_avg = d_dots_sum / data_size;
+
+  std::cout << "s_avg, d_avg, s_dots_avg, d_dots_avg: ";
+  std::cout << s_avg << ", ";
+  std::cout << d_avg << ", ";
+  std::cout << s_dots_avg << ", ";
+  std::cout << d_dots_avg << "\n";
+
+
+
+
   /*
     Trains the classifier with N data points and labels.
 
